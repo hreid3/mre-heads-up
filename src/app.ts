2b11979ef9d95b8bdb1a8056321eaffb7ed815e6
Horace Reid <horace.reid@bluenetreflections.com>
2021-05-10 18:45:05 -0400

    got cubes in place

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,49 @@ import * as MRE from '@microsoft/mixed-reality-extension-sdk';
  * The main class of this Index. All the logic goes here.
  */
 export default class App {
-	private assets: MRE.AssetContainer;
+  private assets: MRE.AssetContainer;
+  private root: MRE.Actor;
 
-	constructor(private context: MRE.Context, private parameterSet: MRE.ParameterSet) {
-		this.context.onStarted(() => this.started());
-	}
+  constructor(private context: MRE.Context, private parameterSet: MRE.ParameterSet) {
+    this.assets = new MRE.AssetContainer(this.context);
+    this.context.onStarted(() => this.started());
+    this.context.onUserJoined(user => {
+    	const redBox =  this.getBlock(this.assets.createMaterial('mat', { color: MRE.Color3.Red() }), {
+				x: 0, y: 0, z: 1.25
+			});
+			redBox.attach(user, 'head');
+			const blueBox =  this.getBlock(this.assets.createMaterial('mat', { color: MRE.Color3.Blue() }), {
+				x: 0, y: 1, z: 1
+			});
+			blueBox.attach(user, 'neck');
+		})
+  }
 
-	private started() {
-		console.log("App Started")
-	}
+  getBlock(mat: MRE.Material, position:  Partial<MRE.Vector3Like>) {
+    const box = this.assets.createBoxMesh('box', 0.5, 0.5, 0.5);
+    const block = MRE.Actor.Create(this.context,
+      {
+        actor: {
+          parentId: this.root.id,
+          appearance: {
+            meshId: box.id,
+            materialId: mat.id,
+          },
+					transform: {
+						local: {
+							position,
+						}
+					},
+				}
+      }
+    );
+    return block;
+  }
+
+  private started() {
+    this.root = MRE.Actor.Create(this.context, { actor: { name: 'Root' } });
+
+    console.log("App Started");
+  }
+
 }
