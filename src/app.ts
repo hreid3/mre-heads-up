86bf332ad8125a0b8d9178a0652da0d3a7baa9c6
Horace Reid <horace.reid@bluenetreflections.com>
2021-05-10 19:39:56 -0400

    head-detech-logic-working

--- src/app.ts
+++ src/app.ts
@@ -16,18 +16,18 @@ export default class App {
     this.assets = new MRE.AssetContainer(this.context);
     this.context.onStarted(() => this.started());
     this.context.onUserJoined(user => {
-    	const redBox =  this.getBlock(this.assets.createMaterial('mat', { color: MRE.Color3.Red() }), {
-				x: 0, y: 0, z: 1.25
-			});
-			redBox.attach(user, 'head');
-			const blueBox =  this.getBlock(this.assets.createMaterial('mat', { color: MRE.Color3.Blue() }), {
-				x: 0, y: 1, z: 1
-			});
-			blueBox.attach(user, 'neck');
-		})
+      const redBox = this.getBlock(this.assets.createMaterial('mat', { color: MRE.Color3.Red() }), {
+        x: 0, y: 0, z: 1.25
+      });
+      redBox.attach(user, 'head');
+      const blueBox = this.getBlock(this.assets.createMaterial('mat', { color: MRE.Color3.Blue() }), {
+        x: 0, y: 1, z: 1
+      });
+      blueBox.attach(user, 'neck');
+    });
   }
 
-  getBlock(mat: MRE.Material, position:  Partial<MRE.Vector3Like>) {
+  getBlock(mat: MRE.Material, position: Partial<MRE.Vector3Like>) {
     const box = this.assets.createBoxMesh('box', 0.5, 0.5, 0.5);
     const block = MRE.Actor.Create(this.context,
       {
@@ -36,15 +36,33 @@ export default class App {
           appearance: {
             meshId: box.id,
             materialId: mat.id,
+            enabled: false, // makes invisible. // Use as the debugger.
           },
-					transform: {
-						local: {
-							position,
-						}
-					},
-				}
+          transform: {
+            local: {
+              position,
+            }
+          },
+          rigidBody: {
+            isKinematic: true,
+          },
+          collider: {
+            geometry: {
+              shape: MRE.ColliderType.Auto,
+              // size: { x: 0.5, y: 0.5, z: 0.5 }
+            },
+            isTrigger: true,
+          }
+        }
       }
     );
+    block.collider.onTrigger('trigger-enter', otherActor => {
+      console.log("Got a hit from another collider")
+    });
+    block.collider.onCollision('collision-enter', otherActor => {
+      console.log("Got a hit from another collider 2")
+    });
+
     return block;
   }
 
