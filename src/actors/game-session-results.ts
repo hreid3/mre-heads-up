de475e310d924537d20df0da204313df4654ad2e
Horace Reid <horace.reid@bluenetreflections.com>
2021-05-17 19:24:43 -0400

    Stable vesion

--- src/actors/game-session-results.ts
+++ src/actors/game-session-results.ts
@@ -1,20 +1,32 @@
 import * as MRE from "@microsoft/mixed-reality-extension-sdk";
+import { AlphaMode, BoxAlignment } from "@microsoft/mixed-reality-extension-sdk";
 import wordwrap from "word-wrap";
 import { ApplicationState, GameSession, PlayedCard } from "../models/application";
 import { AbstractChangeDetection } from "../store/common/AbstractChangeDetection";
 import theme from "../theme/default";
+import delay from "../utils/delay";
 
+const MAX_ITEM_SIZE = 9;
 export class GameSessionResults extends AbstractChangeDetection {
 	private root: MRE.Actor;
 	private assets: MRE.AssetContainer;
 	private actors: MRE.Actor[] = [];
+	private killed = false;
+	private title: MRE.Actor;
+	private internalCount = 0;
+	private generator = 0;
+	private itemCache: MRE.Actor[];
 
 	constructor(private context: MRE.Context, private parent: MRE.Actor) {
 		super();
 		this.assets = new MRE.AssetContainer(this.context);
 	}
 
-	public setup = () => {
+	public setup = async () => {
+		this.killed = false;
+		this.internalCount = 0;
+		this.generator = 0;
+		this.itemCache = [];
 		this.actors = [];
 		const mat = this.assets.createMaterial("mat", {color: theme.color.background.default});
 		const box = this.assets.createBoxMesh("box", 1.6, 1.25, 0.075);
@@ -24,34 +36,81 @@ export class GameSessionResults extends AbstractChangeDetection {
 				parentId: this.parent.id
 			}
 		});
-		const base = this.createBase();
-
-		const textLayout = new MRE.PlanarGridLayout(base);
-		this.getResultsBackground(base, mat, box);
-		const title = this.createTitle(base);
-		this.actors.push(this.root, base, title);
-		let row = 0;
-		textLayout.addCell({
-			row: row++,
-			height: 0.15,
-			column: 0,
-			width: .45,
-			contents: title
-		});
-		for (const card of this.gameSession.draw) {
-			const item = this.createResultItem(base, card);
-			textLayout.addCell({
-				row: row++,
-				height: 0.1,
+		const bg = this.getResultsBackground(this.root, mat, box);
+		const base = this.createBase(bg);
+		this.title = this.createTitle(base);
+		this.actors.push(this.root, base, this.title, bg);
+		const getTextLayout = () => {
+			const layout = new MRE.PlanarGridLayout(base, BoxAlignment.MiddleCenter);
+			layout.addCell({
+				row: 0,
+				height: 0.15,
 				column: 0,
-				width: 2.0,
-				contents: item
+				width: 1.6,
+				contents: this.title,
+				alignment: BoxAlignment.MiddleCenter,
 			});
-			this.actors.push(item);
+			return layout;
 		}
-		textLayout.applyLayout();
+		let textLayout = getTextLayout();
+
+		let row = 0;
+		this.fillAndApplyLayout(base, textLayout);
+		for (const card of this.gameSession.draw) {
+			if (this.killed) { break; }
+			this.internalCount += card.correct ? 1 : 0;
+			await delay(1000);
+			const item = this.createResultItem(base, card);
+			row++;
+			textLayout.addCell({
+				row,
+				height: 0.12,
+				column: 0,
+				width: 1.6,
+				contents: item,
+			});
+			this.itemCache.push(item);
+			this.actors.push(item);
+			if (this.itemCache.length > MAX_ITEM_SIZE) {
+				// Shucks we have work to do
+				const anItem = this.itemCache.shift();
+				anItem.appearance.enabled = false;
+				textLayout = getTextLayout();
+				for(let i=0; i < this.itemCache.length; i++) {
+					textLayout.addCell({
+						row: i + 1,
+						height: 0.12,
+						column: 0,
+						width: 1.6,
+						contents: this.itemCache[i],
+					})
+				}
+			}
+			this.fillAndApplyLayout(base, textLayout);
+			this.title.text.contents = this.getTitleText();
+		}
 	};
 
+	private fillAndApplyLayout = (base: MRE.Actor, layout: MRE.PlanarGridLayout) => {
+		const row = layout.getRowCount();
+		if (row <= MAX_ITEM_SIZE) {
+			for(let i = row; i <= MAX_ITEM_SIZE; i++) {
+				if (this.killed) { break; }
+				// eslint-disable-next-line max-len
+				const item = this.createResultItem(base, {card: { value: ``, id: `idgen_${this.generator++}`, type: 'text'}, correct: false});
+				layout.addCell({
+					row: i,
+					height: 0.12,
+					column: 0,
+					width: 1.6,
+					contents: item,
+					alignment: BoxAlignment.MiddleCenter,
+				});
+				this.actors.push(item);
+			}
+		}
+		layout.applyLayout();
+	}
 	private getResultsBackground = (base: MRE.Actor, mat: MRE.Material, box: MRE.Mesh) => MRE.Actor.Create(this.context,
 		{
 			actor: {
@@ -63,16 +122,13 @@ export class GameSessionResults extends AbstractChangeDetection {
 				},
 				transform: {
 					local: {
-						position: {x: 0.0, y: -0.2, z: 0.0}
+						rotation: {x: -0.258819, y: 0, z: 0},
+						position: {x: 0, z: -0.5, y: 1.2}
 					}
 				},
-				// rigidBody: {
-				// 	isKinematic: true,
-				// },
 				collider: {
 					geometry: {
 						shape: MRE.ColliderType.Auto
-						// size: { x: 0.5, y: 0.5, z: 0.5 }
 					},
 					isTrigger: true
 				}
@@ -80,6 +136,7 @@ export class GameSessionResults extends AbstractChangeDetection {
 		}
 	);
 
+	protected getTitleText = () => `You got ${this.internalCount} right`;
 	protected createTitle = (base: MRE.Actor) => MRE.Actor.Create(this.context, {
 		actor: {
 			parentId: base.id,
@@ -89,12 +146,12 @@ export class GameSessionResults extends AbstractChangeDetection {
 			},
 			transform: {
 				local: {
-					position: {x: 0, y: 0.3, z: -0.05}
+					position: {x: 0, y: 0, z: -0.05}
 				}
 			},
 			text: {
 				pixelsPerLine: 12,
-				contents: `You got ${this.gameSession?.correctCount} right`,
+				contents: this.getTitleText(),
 				height: 0.070,
 				anchor: MRE.TextAnchorLocation.MiddleCenter,
 				justify: MRE.TextJustify.Center,
@@ -118,7 +175,7 @@ export class GameSessionResults extends AbstractChangeDetection {
 			text: {
 				pixelsPerLine: 12,
 				contents: wordwrap(`${card.value}`, {width: 140}),
-				height: 0.06,
+				height: 0.065,
 				anchor: MRE.TextAnchorLocation.MiddleCenter,
 				justify: MRE.TextJustify.Center,
 				color: correct ? theme.color.font.paragraph : theme.color.font.disabled
@@ -126,18 +183,24 @@ export class GameSessionResults extends AbstractChangeDetection {
 		}
 	});
 
-	protected createBase = () => MRE.Actor.Create(this.context,
-		{
-			actor: {
-				parentId: this.root.id,
-				transform: {
-					local: {
-						rotation: {x: -0.258819, y: 0, z: 0},
-						position: {x: 0, z: -0.5, y: 1.2}
+	protected createBase = (parent: MRE.Actor) => {
+		const box = this.assets.createBoxMesh('box', 1.6, 1, 0.075);
+		const mat = this.assets.createMaterial('transparent', {
+			color: MRE.Color4.FromColor3(MRE.Color3.White(), 0),
+			alphaMode: AlphaMode.Blend,
+		})
+		return MRE.Actor.Create(this.context,
+			{
+				actor: {
+					parentId: parent.id,
+					appearance: { meshId: box.id, materialId: mat.id },
+					transform: {
+						local: {
+						}
 					}
 				}
-			}
-		});
+			});
+	}
 
 	handleApplicationStateChanged(prev: ApplicationState): void {
 		if (prev.displayResults !== this.applicationState.displayResults) {
@@ -153,6 +216,7 @@ export class GameSessionResults extends AbstractChangeDetection {
 	}
 
 	public destroy = () => {
+		this.killed = true;
 		this.actors.forEach(v => v.destroy());
 	};
 }
