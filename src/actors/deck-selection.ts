d6e7a8b6a8e0d49e6c1a33f9dc6a39789a337e8c
Horace <horace.reid@bluenetreflections.com>
2021-05-22 19:32:13 -0400

    Stable deck cards

--- src/actors/deck-selection.ts
+++ src/actors/deck-selection.ts
@@ -6,10 +6,11 @@ import { ApplicationManager, Deck, DecksState, GAME_STATE, GameSession, ID } fro
 import { playerDeckCanceled, playerDeckSelected } from "../store/app/actions";
 import { setFlipDeck } from "../store/decks/actions";
 import theme from "../theme/default";
+import noop from "../utils/noop";
 
 const playButtonName = "playButton";
 const playButtonLabel = "label";
-const DECK_CARD_PREFIX = 'deckCard_';
+const DECK_CARD_PREFIX = "deckCard_";
 
 const getButtonLabel =
 	(actor: MRE.Actor) => actor.children.find(v => v.name === playButtonLabel);
@@ -50,44 +51,6 @@ export class DeckSelection {
 	};
 
 	public setOnlyDeckSelected = (value: boolean, selectedDeckId?: ID) => {
-		// const setBtnLbl = (actor: MRE.Actor, label: string, setDefaultColor?: boolean) => {
-		// 	const children = actor.children;
-		// 	for (const child of children) {
-		// 		if (child.name === playButtonName) {
-		// 			const grandChildren = child.children;
-		// 			for (const grandChild of grandChildren) {
-		// 				if (grandChild.name === playButtonLabel) {
-		// 					grandChild.text.contents = label;
-		// 					if (setDefaultColor) {
-		// 						grandChild.text.color = theme.color.button.default.text;
-		// 					}
-		// 				}
-		// 			}
-		// 			if (setDefaultColor) {
-		// 				child.appearance.material.color = MRE.Color4.FromColor3(theme.color.button.default.background);
-		// 			}
-		// 		}
-		// 	}
-		// };
-		// if (this.decksState.decks) {
-		// 	if (value && selectedDeckId) {
-		// 		for (const deck of this.decksState.decks) {
-		// 			const actor = this.actorDeckMapping[deck.id];
-		// 			actor.appearance.enabled = selectedDeckId === deck.id;
-		// 			setBtnLbl(actor, "Cancel");
-		// 			this.layoutCards([actor]);
-		// 		}
-		// 	} else if (!this.deckCards?.length) {
-		// 		const deckCards: Actor[] = [];
-		// 		for (const deck of this.decksState.decks) {
-		// 			const actor = this.actorDeckMapping[deck.id];
-		// 			deckCards.push(actor);
-		// 			actor.appearance.enabled = deck.enabled;
-		// 			setBtnLbl(actor, "Play", true);
-		// 		}
-		// 		this.layoutCards(deckCards);
-		// 	}
-		// }
 	};
 
 	protected detectChanges = () => {
@@ -99,8 +62,6 @@ export class DeckSelection {
 				if (!this.deckCards?.length) {
 					this.setup();
 				}
-				// this.destroy();
-				// this.setup();
 			}
 			if (this.gameSession !== gameSession) {
 				const prev = this.gameSession;
@@ -213,7 +174,7 @@ export class DeckSelection {
 	private getDeckPrefab = async (deck: Deck, base: MRE.Actor) => {
 		const loader = await this.assetContainer.loadGltf(deck.prefabUri, "box");
 		const prefab = loader.find(a => a.prefab !== null)?.prefab;
-		if(!prefab) {
+		if (!prefab) {
 			throw Error(`No prefeb defined for ${deck.name}`);
 		}
 		return MRE.Actor.CreateFromPrefab(this.appManager.getContext(),
@@ -224,7 +185,7 @@ export class DeckSelection {
 					parentId: base.id,
 					transform: {
 						local: {
-							position: { x: 0.0, y: 0.0, z: 0.0 }
+							position: {x: 0.0, y: 0.0, z: 0.0}
 						}
 					},
 					collider: {
@@ -236,9 +197,8 @@ export class DeckSelection {
 				}
 			}
 		);
-	}
+	};
 
-	// Todo: This qualifies for a Atomic Component
 	private getPlayButton = (base: MRE.Actor, deck: Deck) => {
 		const mat = this.assets.createMaterial("mat", {color: theme.color.button.default.background});
 		const box = this.assets.createBoxMesh("box", 0.22, 0.075, 0.0005);
@@ -255,29 +215,29 @@ export class DeckSelection {
 					transform: {
 						local: {
 							position: {x: -0.122, y: -0.27, z: 0.0015},
-							rotation: base.transform.local.rotation,
+							rotation: base.transform.local.rotation
 						}
 					},
 					collider: {
 						geometry: {
 							shape: MRE.ColliderType.Auto
-							// size: { x: 0.5, y: 0.5, z: 0.5 }
 						},
 						isTrigger: true
 					}
 				}
 			}
 		);
-		playButton.collider.onTrigger("trigger-enter", () => {
-		});
-		console.log("Horace", { ...base.transform.local.rotation })
+		playButton.collider.onTrigger("trigger-enter", noop);
 		MRE.Actor.Create(this.appManager.getContext(), {
 			actor: {
 				name: playButtonLabel,
 				parentId: playButton.id,
-				transform: {local: {position: {z: 0.005, y: 0},
-					rotation: { y: 45 },
-				}},
+				transform: {
+					local: {
+						position: {z: 0.005, y: 0},
+						rotation: {y: 45}
+					}
+				},
 				text: {
 					contents: "Play",
 					pixelsPerLine: 12,
@@ -292,10 +252,12 @@ export class DeckSelection {
 	};
 
 	public attachBehaviors = () => {
-		for (const deck of this.decksState.decks) {
-			if (deck.enabled) {
-				this.attachPlayButtonBehaviors(deck);
-				this.attachDeckCardBehaviors(deck);
+		if (this.decksState?.decks) {
+			for (const deck of this.decksState.decks) {
+				if (deck.enabled) {
+					this.attachPlayButtonBehaviors(deck);
+					this.attachDeckCardBehaviors(deck);
+				}
 			}
 		}
 	};
@@ -310,18 +272,35 @@ export class DeckSelection {
 
 		});
 		deckCardBehavior.onClick(user => {
-			const updatedDeck =
-				this.decksState.decks.filter(value => value.enabled).find(value => value.id === deck.id);
-			if (!updatedDeck?.flipped) {
-				// Flip the card and activate the playbutton
-				deckBase.transform.local.rotation.y = 90;
-			} else {
-				deckBase.transform.local.rotation.y = 0;
-				this.appManager.getStore().dispatch(setFlipDeck({id: deck.id, flipped: false }))
+			if (this.gameSession.state === GAME_STATE.Waiting) {
+				const scaleFactor = 1.5;
+				const lastFippedCard = this.decksState.decks.find(value => value.flipped);
+				const lastFlippedCardId = lastFippedCard?.id;
+				if (lastFlippedCardId) { // Flip the existing card
+					this.appManager.getStore().dispatch(setFlipDeck({id: lastFlippedCardId, flipped: false}));
+					const otherCardBase = this.deckCards
+						.find(value => `${DECK_CARD_PREFIX}${lastFippedCard.name}` === value.name);
+					// TODO: Kevin to Animate
+					otherCardBase.transform.local.rotation.y = 0;
+					otherCardBase.transform.local.position.z = 0;
+					otherCardBase.transform.local.position.z = 0;
+					otherCardBase.transform.local.scale.x = otherCardBase.transform.local.scale.x / scaleFactor;
+					otherCardBase.transform.local.scale.y = otherCardBase.transform.local.scale.y / scaleFactor;
+					otherCardBase.transform.local.scale.z = otherCardBase.transform.local.scale.z / scaleFactor;
+				}
+				if (lastFlippedCardId !== deck.id) {
+					// TODO: Kevin to Animate
+					deckBase.transform.local.rotation.y = 90;
+					deckBase.transform.local.position.z = -0.2;
+					deckBase.transform.local.scale.x = deckBase.transform.local.scale.x * scaleFactor;
+					deckBase.transform.local.scale.y = deckBase.transform.local.scale.y * scaleFactor;
+					deckBase.transform.local.scale.z = deckBase.transform.local.scale.z * scaleFactor;
+
+					this.appManager.getStore().dispatch(setFlipDeck({id: deck.id, flipped: true}));
+				}
 			}
-			this.appManager.getStore().dispatch(setFlipDeck({id: deck.id, flipped: !updatedDeck.flipped }))
-		})
-	}
+		});
+	};
 
 	protected attachPlayButtonBehaviors = (deck: Deck) => {
 		try {
@@ -345,10 +324,10 @@ export class DeckSelection {
 				if (this.gameSession.state === GAME_STATE.Playing) {
 					// TODO: Prompt to be sure.
 					this.appManager.getStore().dispatch(playerDeckCanceled({playerId: user.id.toString()}));
-					label.text.contents = 'Play';
+					label.text.contents = "Play";
 				} else {
 					this.root.startSound(this.playButtonSoundAsset?.id, {...config.soundOptions});
-					label.text.contents = 'Cancel';
+					label.text.contents = "Cancel";
 					this.appManager.getStore().dispatch(playerDeckSelected({
 						selectedDeckId: deck.id,
 						playerId: user.id.toString()
@@ -358,7 +337,7 @@ export class DeckSelection {
 		} catch (error) {
 			console.error("Error attaching behaviors for", deck);
 		}
-	}
+	};
 
 	// eslint-disable-next-line max-len
 	private getDeckBackground = (base: MRE.Actor, mat: MRE.Material, box: MRE.Mesh) => MRE.Actor.Create(this.appManager.getContext(),
@@ -375,9 +354,6 @@ export class DeckSelection {
 						position: {x: 0.0, y: -0.1, z: 0.0}
 					}
 				},
-				// rigidBody: {
-				// 	isKinematic: true,
-				// },
 				collider: {
 					geometry: {
 						shape: MRE.ColliderType.Auto
@@ -390,44 +366,22 @@ export class DeckSelection {
 	);
 
 	private createDeck = (deck: Deck) => {
-		// const mat = this.assets.createMaterial("mat", {color: theme.color.background.default});
-		// const box = this.assets.createBoxMesh("box", 0.8, 1, 0.075);
 		const base = MRE.Actor.Create(
 			this.appManager.getContext(), {
 				actor: {
 					name: `${DECK_CARD_PREFIX}${deck.name}`,
 					parentId: this.root.id,
-					transform: { local: { rotation: { y: deck.flipped ? 9 : 0 } },},
+					transform: {local: {rotation: {y: deck.flipped ? 9 : 0}}},
 					collider: {
 						geometry: {
 							shape: MRE.ColliderType.Auto
-							// size: { x: 0.5, y: 0.5, z: 0.5 }
 						},
 						isTrigger: true
 					}
-				}});
-		// this.getDeckBackground(base, mat, box);
-		this.getDeckPrefab(deck, base)
-		// const title = this.getDeckTitle(deck, base, box, mat);
-		// const description = this.getDeckDesc(deck, base, box, mat);
+				}
+			});
+		this.getDeckPrefab(deck, base);
 		this.getPlayButton(base, deck);
-		// const textLayout = new MRE.PlanarGridLayout(base);
-		// textLayout.addCell({
-		// 	row: 0,
-		// 	height: 0.0,
-		// 	column: 0,
-		// 	width: .45,
-		// 	contents: title
-		// });
-		//
-		// textLayout.addCell({
-		// 	row: 1,
-		// 	height: 0.4,
-		// 	column: 0,
-		// 	width: .45,
-		// 	contents: description
-		// });
-		// textLayout.applyLayout();
 		return base;
 	};
 }
